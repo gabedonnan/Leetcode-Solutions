bb06a7f9302eeb76f05efc673eb4d05ea98a4d2a
Gabriel Donnan <47415809+gabedonnan@users.noreply.github.com>
2023-01-15 17:15:02 +0000

    Create determine-matrix-obtainable.py
    Determines whether the target matrix is obtainable by some rotation of the original matrix.

---
+++ determine-matrix-obtainable.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        for i in range(4):
+            if mat == target:
+                return True
+            mat = self.rotOnce(mat)
+        return False
+
+    def rotOnce(self, matrix: List[List[int]]) -> List[List[int]]:
+        return [[matrix[-(j+1)][i] for j in range(len(row))] for i,row in enumerate(matrix)]
